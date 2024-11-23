8d54b7d8bb9466afaa5e38e23445e539bc90a514
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2024-11-22 21:08:45 -0500

    feat: add suppot for cors-headers

--- backend/SeniorCareMedBackend/settings.py
+++ backend/SeniorCareMedBackend/settings.py
@@ -69,7 +69,9 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:4173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:4173",
 ]
 
 SIMPLE_JWT = {
